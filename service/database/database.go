c6293b7ebe7465cbd3e2993673171fe96d95f363
james <francescojamesfanti@gmail.com>
2023-12-23 03:13:32 +0100

    ecc

--- service/database/database.go
+++ service/database/database.go
@@ -34,13 +34,12 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-
-	"github.com/neoSnakex34/WasaPhoto/service/api/objs"
+	// "github.com/neoSnakex34/WasaPhoto/service/struct/objs"
 )
 
 // AppDatabase is the high level interface for the DB
 type AppDatabase interface {
-	DoLogin(username string) (objs.Identifier, error)
+	// DoLogin(username string) (objs.Identifier, error)
 	GetName() (string, error)
 	SetName(name string) error
 
