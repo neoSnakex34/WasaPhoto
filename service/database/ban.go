ffacb80c27df74bf2bcf0410c3357d6022489c0e
Francesco James Fanti <97003970+neoSnakex34@users.noreply.github.com>
2024-04-03 02:55:11 +0200

    updt

--- service/database/ban.go
+++ service/database/ban.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"github.com/neoSnakex34/service/structs"
+	"github.com/neoSnakex34/WasaPhoto/service/structs"
 )
 func (db *appdbimpl) BanUser(bannerId, bannedId structs.Identifier) error {
 	_, err := db.Exec("INSERT INTO banTable (banner_id, banned_id) VALUES (?, ?)", bannerId, bannedId)
