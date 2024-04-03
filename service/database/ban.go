35812f6eeeb33825473a3958b02bec5ba2742ace
Francesco James Fanti <97003970+neoSnakex34@users.noreply.github.com>
2024-04-03 02:45:23 +0200

    sssss

--- service/database/ban.go
+++ service/database/ban.go
@@ -1,5 +1,8 @@
 package database
 
+import (
+	"github.com/neoSnakex34/service/structs"
+)
 func (db *appdbimpl) BanUser(bannerId, bannedId structs.Identifier) error {
 	_, err := db.Exec("INSERT INTO banTable (banner_id, banned_id) VALUES (?, ?)", bannerId, bannedId)
 	if err != nil {
@@ -7,5 +10,5 @@ func (db *appdbimpl) BanUser(bannerId, bannedId structs.Identifier) error {
 	}
 
 	return nil
-	
+
 } 
