274eb02d7ecb39358314bd2f25aa18baf6462c24
james <francescojamesfanti@gmail.com>
2024-04-05 12:51:57 +0200

    interactions + small changes on apis

--- service/database/interactions.go
+++ service/database/interactions.go
@@ -1,5 +1,22 @@
+package database
+
+import (
+	"time"
+
+	"github.com/neoSnakex34/WasaPhoto/service/structs"
+)
 
 // IMPORTANT likeid = linkingUser;
 
 // ======= comments operations
-func (db *appdbimpl) CommentPhoto()
+func (db *appdbimpl) CommentPhoto(photoId structs.Identifier, userId structs.Identifier) (structs.Comment, error) {
+
+	// TODO generate valid commentId
+
+	// TODO keep the date inference after the validId loop
+
+	date := time.Now().Format(time.RFC3339)
+	// FIXME just delete this placeholder after finishing the function
+	println(date)
+	return structs.Comment{}, nil
+}
