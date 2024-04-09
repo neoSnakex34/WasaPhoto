e46e78752aee3cab62fcd743095211d80da84f0c
james <francescojamesfanti@gmail.com>
2024-04-09 09:44:26 +0200

    updates

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -10,6 +10,33 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.GET("/", rt.getHelloWorld)
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
+	// session login routes
+	rt.router.POST("/login", rt.wrap(rt.doLogin))
+
+	// userProfile and stream related routes
+	rt.router.GET("/users/:userId/profile", rt.wrap(rt.getUserProfile))
+	rt.router.GET("/users/:userId/stream", rt.wrap(rt.getMyStream))
+
+	// ban related routes
+	rt.router.PUT("/users/:userId/bans/:bannedId", rt.wrap(rt.banUser))
+	rt.router.DELETE("/users/:userId/bans/:bannedId", rt.wrap(rt.unbanUser))
+
+	// follow related routes
+	rt.router.PUT("/users/:userId/follows/:followerId", rt.wrap(rt.followUser))
+	rt.router.DELETE("/users/:userId/follows/:followerId", rt.wrap(rt.unfollowUser))
+
+	// photo related routes
+	rt.router.POST("/users/:userId/photos/:photoId", rt.wrap(rt.uploadPhoto))
+	rt.router.DELETE("/users/:userId/photos/:photoId", rt.wrap(rt.deletePhoto))
+
+	// comment related routes
+	rt.router.POST("/users/:userId/photos/:photoId/comments/:commentId", rt.wrap(rt.commentPhoto))
+	rt.router.DELETE("/users/:userId/photos/:photoId/comments/:commentId", rt.wrap(rt.uncommentPhoto))
+
+	// like related routes
+	rt.router.PUT("/users/photos/:photoId/likes/:userId", rt.wrap(rt.likePhoto))
+	rt.router.DELETE("/users/photos/:photoId/likes/:userId", rt.wrap(rt.unlikePhoto))
+
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
 
